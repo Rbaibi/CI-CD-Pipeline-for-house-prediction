0564effb84342ecf55206f26c84584cc452a36a3
Rbaibi <Ahmed.Rbaibi@gmail.com>
2022-10-13 16:14:23 +0200

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,1 +1,29 @@
+from locust import HttpUser, task
+
+class TestUser(HttpUser):
+    @task
+    def connect(self):
+        self.client.get("/")
+    @task
+    def predict(self):
+        self.client.post("/predict", json={
+    "CHAS": {
+        "0": 0
+    },
+    "RM": {
+        "0": 6.575
+    },
+    "TAX": {
+        "0": 296.0
+    },
+    "PTRATIO": {
+        "0": 15.3
+    },
+    "B": {
+        "0": 396.9
+    },
+    "LSTAT": {
+        "0": 4.98
+    }
+    })
 
